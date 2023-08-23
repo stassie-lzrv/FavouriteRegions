44376bda53c79ad7c2e535575e7da4b4916b2abc
stassie-lzrv <adlazareva@edu.hse.ru>
2023-08-23 23:11:00 +0300

    refactor: removed storyboard + added gitignore

--- FavouriteRegions/HomeViewController.swift
+++ FavouriteRegions/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  FavouriteRegions
 //
 //  Created by Настя Лазарева on 23.08.2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
